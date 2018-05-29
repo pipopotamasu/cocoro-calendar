38774c816d1ceb20b5442df6489a26c490db8b60
pipopotamasu <ec0156hx@gmail.com>
2018-05-29 21:39:59 +0900

    WIP

--- src/components/CalendarScreen/CalendarScreen.js
+++ src/components/CalendarScreen/CalendarScreen.js
@@ -3,14 +3,68 @@ import { Container, Content } from "native-base";
 import { StyleSheet } from 'react-native';
 import GlobalHeader from "../GlobalHeader";
 import { Calendar } from 'react-native-calendars';
+import AppStore from "../../store/appStore";
 
 export default class CalendarScreen extends React.Component {
-  render() {
+  componentWillMount() {
+    // AppStore.registerTodosGroupByDate()
+  }
+
+  async markedColorDates () {
+    const todos_group_by_day = await AppStore.fetchTodosGroupByDate()
+    console.log(todos_group_by_day)
+    let datesWithColor = {}
+    for (let date in todos_group_by_day) {
+      let progress = this.calProgress(todos_group_by_day[date])
+      dateWithColor = { [date]: { color: this.calColor(progress) } }
+      datesWithColor = Object.assign(datesWithColor, dateWithColor)
+    }
+    return datesWithColor
+  }
+
+  calProgress (todos) {
+    // if(progress) return progress
+    const doneCount = todos.filter((todo) => { return (todo.done) }).length
+    progress = doneCount / 5
+    return progress
+  }
+
+  calColor (progress) {
+    switch (progress) {
+      case 0:
+        return 'rgb(255,255,255)'
+        break;
+      case 0.2:
+        return 'rgb(0,256,0)'
+        break;
+      case 0.4:
+        return 'rgb(0,220,0)'
+        break;
+      case 0.6:
+        return 'rgb(0,190,0)'
+        break;
+      case 0.8:
+        return 'rgb(0,160,0)'
+        break;
+      case 1:
+        return 'rgb(0,128,0)'
+        break;
+    }
+  }
+
+  async render() {
+    const hoge = await this.markedColorDates()
+    console.log(hoge)
     return (
       <Container>
         <GlobalHeader title="Calendar" navigation={this.props.navigation}/>
         <Content padder style={styles.calendar}>
-          <Calendar/>
+          <Calendar
+            markedDates={
+              hoge
+            }
+            markingType={'period'}
+          />
         </Content>
       </Container>
     );
--- src/store/appStore.js
+++ src/store/appStore.js
@@ -1,4 +1,4 @@
-import { toJS, observable } from 'mobx';
+import { observable } from 'mobx';
 import Store from 'react-native-store';
 
 const INITIAL_TODOS = [{ id: 1, text: 'test1', done: false, created_at: null },
@@ -14,6 +14,7 @@ const DB = {
 class AppStore {
   @observable today = today();
   @observable todos = [];
+
   get todosProgress() {
     const doneCount = this.todos.filter((todo) => { return (todo.done) }).length
     progress = doneCount / 5
@@ -42,6 +43,22 @@ class AppStore {
       // fetch today's todos if exists
       this.todos = res
     }
+  }
+
+  fetchTodosGroupByDate = async () => {
+    const today = new Date()
+    const year = today.getFullYear()
+    const month = ( "0" + ( today.getMonth() + 1 )).slice(-2)
+    let todos_group_by_day = {}
+    for(let i = 1; i <= 31; i++) {
+      const day = ( '0' + i ).slice(-2)
+      const fullYMD = `${year}-${month}-${day}`
+      const res = await this.fetchTodos(fullYMD)
+
+      if (res) todos_group_by_day = Object.assign(todos_group_by_day, { [fullYMD]: res })
+    }
+
+    return todos_group_by_day
   }
 
   async fetchTodos(createdAt) {
