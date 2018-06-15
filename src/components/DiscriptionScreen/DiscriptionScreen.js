8ca591929013ae37007788697eed6b8b1b2b853b
pipopotamasu <ec0156hx@gmail.com>
2018-06-15 23:34:54 +0900

    picker

--- src/components/DiscriptionScreen/DiscriptionScreen.js
+++ src/components/DiscriptionScreen/DiscriptionScreen.js
@@ -1,19 +1,37 @@
 import React from "react";
-import { Container, Header, Title, Left, Icon, Right, Button, Body, Content,Text, Card, CardItem } from "native-base";
+import { Container, Header, Title, Left, Icon, Right, Button, Body, Content,Text, Card, CardItem, Picker } from "native-base";
 import GlobalHeader from "../GlobalHeader";
 export default class DiscriptionScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selected1: "key1"
+    };
+  }
+
+  onValueChange(value) {
+    this.setState({
+      selected1: value
+    });
+  }
+
   render() {
     return (
       <Container>
         <GlobalHeader title="Discription" navigation={this.props.navigation}/>
         <Content padder>
-          <Card>
-            <CardItem>
-              <Body>
-                <Text>Chat App to talk some awesome people!</Text>
-              </Body>
-            </CardItem>
-          </Card>
+          <Picker
+                iosHeader="Select one"
+                mode="dropdown"
+                selectedValue={this.state.selected1}
+                onValueChange={this.onValueChange.bind(this)}
+              >
+            <Picker.Item label="Wallet" value="key0" />
+            <Picker.Item label="ATM Card" value="key1" />
+            <Picker.Item label="Debit Card" value="key2" />
+            <Picker.Item label="Credit Card" value="key3" />
+            <Picker.Item label="Net Banking" value="key4" />
+          </Picker>
         </Content>
       </Container>
     );
