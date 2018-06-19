fa294d0b1b0c11bb5f93d3bd8a6cde2e9fb29cac
pipopotamasu <ec0156hx@gmail.com>
2018-06-20 02:03:34 +0900

    remove sidebar

--- src/components/HomeScreen/index.js
+++ src/components/HomeScreen/index.js
@@ -1,13 +1,9 @@
 import React, { Component } from "react";
 import TabScreen from "../TabScreen/TabScreen.js";
-import SideBar from "../SideBar/SideBar.js";
 import { DrawerNavigator } from "react-navigation";
 const HomeScreenRouter = DrawerNavigator(
   {
     Home: { screen: TabScreen }
-  },
-  {
-    contentComponent: props => <SideBar {...props} />
   }
 );
 export default HomeScreenRouter;
