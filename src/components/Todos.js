ad7914bfbf0f477c1d5b231b00ead2832b190b31
pipopotamasu <ec0156hx@gmail.com>
2018-06-22 00:22:39 +0900

    wip

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -12,7 +12,14 @@ export default Todos = (props) => {
         <ListItem onPress={()=>AppStore.toggleDone(item.id)}>
           <CheckBox onPress={()=>AppStore.toggleDone(item.id)} checked={item.done}/>
           <Text style={styles.itemText}>{item.text}</Text>
-          <Icon onPress={()=>props.navigation.navigate("Description")} style={styles.icon} type="MaterialCommunityIcons" name="comment-question-outline" />
+          <Icon
+            onPress={ () => {
+              // update discription state
+              props.navigation.navigate("Description")
+            }}
+            style={styles.icon}
+            type="MaterialCommunityIcons"
+            name="comment-question-outline" />
         </ListItem>
       )}
     />
